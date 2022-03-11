1b6f35a676d1ac1bb6fe0cd6a6775f0a73d56506
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-03-11 14:43:50 -0300

    feat: finished chapter 1

--- src/bin/chapter_1.rs
+++ src/bin/chapter_1.rs
@@ -1,45 +1,49 @@
-// use raytracer::{Point, Vector};
+use std::thread;
+use raytracer::tuple::{Point, Vector};
 
-// Putting It Together
-//
-// Try playing with this little program, firing virtual projectiles and seeing how fat they go.
-// It'll let you exercise the vector and point routines you've written. Start with the following
-// two data structures:
-//
-//  * A projectile has a position (a point) and a velocity (a vector).
-//  * An environment has gravity (a vector) and wind (a vector).
-//
-// Then, add a `tick(environment, projectile)` function which returns a new projectile,
-// representing the given projectile after one unit of time has passed. (The actual units here
-// don't really matter - maybe they're seconds, or milliseconds. Whatever. We'll just call them
-// "ticks".)
+#[derive(Copy, Clone, Debug)]
+struct Projectile {
+    position: Point,
+    velocity: Vector,
+}
 
-// struct Projectile {
-//     position: Point,
-//     velocity: Vector,
-// }
-// 
-// struct Environment {
-//     gravity: Vector,
-//     wind: Vector,
-// }
-// 
-// fn tick(env: Environment, proj: Projectile) -> Projectile {
-//     let position = proj.position + proj.velocity;
-//     let velocity = proj.velocity + env.gravity + env.wind;
-//     Projectile { position, velocity }
-// }
-// 
-// fn main() {
-//     let proj = Projectile {
-//         position: Point::new(0., 1., 0.),
-//         velocity: Vector::new(1., 1., 0.).normalize(),
-//     };
-// 
-//     let env = Environment {
-//         gravity: Vector::new(0., -0.1, 0.),
-//         wind: Vector::new(-0.01, 0., 0.),
-//     };
-// 
-//     tick(env, proj);
-// }
+#[derive(Copy, Clone)]
+struct Environment {
+    gravity: Vector,
+    wind: Vector,
+}
+
+fn tick(env: Environment, proj: &mut Projectile) {
+    proj.position = proj.position + proj.velocity;
+    proj.velocity = proj.velocity + env.gravity + env.wind;
+}
+
+fn main() {
+    let mut proj = Projectile {
+        position: Point::new(0., 1., 0.),
+        velocity: Vector::new(1., 1., 0.).normalize(),
+    };
+
+    let env = Environment {
+        gravity: Vector::new(0., -0.1, 0.),
+        wind: Vector::new(-0.01, 0., 0.),
+    };
+
+    let mut ticks = 0;
+
+    loop {
+        tick(env, &mut proj);
+        ticks += 1;
+
+        println!(
+            "\rProjectile {{ x: {:.2}, y: {:.2} }}",
+            proj.position.x, proj.position.y
+        );
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        if proj.position.y <= 0. {
+            break;
+        }
+    }
+}
