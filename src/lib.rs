2cc02ec619b6b87ea0295b3c27f5bb105a7c0ee9
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-03-10 21:05:49 -0300

    chapter 1: implemented type restrictions for addition and substraction

--- src/lib.rs
+++ src/lib.rs
@@ -2,49 +2,119 @@ use std::cmp::PartialEq;
 use std::ops::{Add, Sub};
 
 #[derive(Debug)]
-struct Tuple {
+struct Point {
     x: f64,
     y: f64,
     z: f64,
-    w: u8,
 }
 
-impl Tuple {
-    fn point(x: f64, y: f64, z: f64) -> Self {
-        Self { x, y, z, w: 1 }
-    }
+#[derive(Debug)]
+struct Vector {
+    x: f64,
+    y: f64,
+    z: f64,
+}
 
-    fn vector(x: f64, y: f64, z: f64) -> Self {
-        Self { x, y, z, w: 0 }
+impl Point {
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
     }
 }
 
-impl PartialEq for Tuple {
+impl Vector {
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+}
+
+fn compare_floating_points(a: f64, b: f64) -> bool {
+    const EPSILON: f64 = 0.00001;
+    a.abs() - b.abs() < EPSILON
+}
+
+impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
-        const EPSILON: f64 = 0.00001;
-        let compare_floating_points = |a: f64, b: f64| a.abs() - b.abs() < EPSILON;
-
         compare_floating_points(self.x, other.x)
             && compare_floating_points(self.y, other.y)
             && compare_floating_points(self.z, other.z)
-            && self.w == other.w
     }
 }
 
-impl Add for Tuple {
+impl PartialEq for Vector {
+    fn eq(&self, other: &Self) -> bool {
+        compare_floating_points(self.x, other.x)
+            && compare_floating_points(self.y, other.y)
+            && compare_floating_points(self.z, other.z)
+    }
+}
+
+// Point + Vector -> Point
+impl Add<Vector> for Point {
     type Output = Self;
+
+    fn add(self, other: Vector) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+// Vector + Point -> Point
+impl Add<Point> for Vector {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+// Vector + Vector -> Vector
+impl Add<Vector> for Vector {
+    type Output = Vector;
 
     fn add(self, other: Self) -> Self {
         Self {
             x: self.x + other.x,
             y: self.y + other.y,
             z: self.z + other.z,
-            w: self.w + other.w,
         }
     }
 }
 
-impl Sub for Tuple {
+// Point - Point -> Vector
+impl Sub<Point> for Point {
+    type Output = Vector;
+
+    fn sub(self, other: Self) -> Vector {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+// Point - Vector -> Point
+impl Sub<Vector> for Point {
+    type Output = Self;
+
+    fn sub(self, other: Vector) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+// Vector - Vector -> Vector
+impl Sub<Vector> for Vector {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
@@ -52,7 +122,6 @@ impl Sub for Tuple {
             x: self.x - other.x,
             y: self.y - other.y,
             z: self.z - other.z,
-            w: self.w - other.w,
         }
     }
 }
@@ -62,69 +131,53 @@ mod tests {
     use super::*;
 
     #[test]
-    fn points_are_created_with_w_1() {
-        let point = Tuple::point(4.0, -4.0, 3.0);
-        let expected = Tuple {
-            x: 4.0,
-            y: -4.0,
-            z: 3.0,
-            w: 1,
-        };
-
-        assert_eq!(point, expected);
+    fn points_are_created_desired_values() {
+        let point = Point::new(4.0, -4.0, 3.0);
+        assert_eq!(point.x, 4.0);
+        assert_eq!(point.y, -4.0);
+        assert_eq!(point.z, 3.0);
     }
 
     #[test]
-    fn vectors_are_created_with_w_0() {
-        let vector = Tuple::vector(4.0, -4.0, 3.0);
-        let expected = Tuple {
-            x: 4.0,
-            y: -4.0,
-            z: 3.0,
-            w: 0,
-        };
-
-        assert_eq!(vector, expected);
+    fn vectors_are_created_desired_values() {
+        let vector = Vector::new(4.0, -4.0, 3.0);
+        assert_eq!(vector.x, 4.0);
+        assert_eq!(vector.y, -4.0);
+        assert_eq!(vector.z, 3.0);
     }
 
     #[test]
-    fn adding_two_tupples() {
-        let tuple1 = Tuple {
-            x: 3.0,
-            y: -2.0,
-            z: 5.0,
-            w: 1,
-        };
-        let tuple2 = Tuple {
-            x: -2.0,
-            y: 3.0,
-            z: 1.0,
-            w: 0,
-        };
-        let expected = Tuple {
-            x: 1.0,
-            y: 1.0,
-            z: 6.0,
-            w: 1,
-        };
+    fn adding_two_vectors() {
+        let vector1 = Vector::new(3.0, -2.0, 5.0);
+        let vector2 = Vector::new(-2.0, 3.0, 1.0);
+        let expected = Vector::new(1.0, 1.0, 6.0);
 
-        assert_eq!(tuple1 + tuple2, expected);
+        assert_eq!(vector1 + vector2, expected);
+    }
+
+    #[test]
+    fn adding_point_and_vector() {
+        let point = Point::new(3.0, 2.0, 1.0);
+        let vector = Vector::new(5.0, 6.0, 7.0);
+        let expected = Point::new(8.0, 10.0, 8.0);
+
+        assert_eq!(point + vector, expected);
     }
 
     #[test]
     fn subtracting_two_points() {
-        let point1 = Tuple::point(3.0, 2.0, 1.0);
-        let point2 = Tuple::point(5.0, 6.0, 7.0);
-        let expected = Tuple::vector(-2.0, -4.0, -6.0);
+        let point1 = Point::new(3.0, 2.0, 1.0);
+        let point2 = Point::new(5.0, 6.0, 7.0);
+        let expected = Vector::new(-2.0, -4.0, -6.0);
 
         assert_eq!(point1 - point2, expected);
     }
 
     #[test]
     fn subtracting_vector_from_point() {
-        let point = Tuple::point(3.0, 2.0, 1.0);
-        let vector = Tuple::vector(5.0, 6.0, 7.0);
-        let expected = Tuple::point(-2.0, -4.0, -6.0);
+        let point = Point::new(3.0, 2.0, 1.0);
+        let vector = Vector::new(5.0, 6.0, 7.0);
+        let expected = Point::new(-2.0, -4.0, -6.0);
 
         assert_eq!(point - vector, expected);
     }
