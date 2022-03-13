756cee581964e879c557a5b91ae666079d1801ef
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-03-13 10:38:33 -0300

    feat: implementing ops on &Tuple instead of Tuple

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,20 @@
 use std::ops::Add;
 
-const EPSILON: f64 = 0.00001;
-
 #[derive(Copy, Clone, Debug)]
 struct Tuple(f64, f64, f64);
 
 impl PartialEq for Tuple {
     fn eq(&self, other: &Self) -> bool {
-        let self_fields = [self.0, self.1, self.2];
-        let other_fields = [other.0, other.1, other.2];
-        let fields = self_fields.iter().zip(other_fields);
-
-        for (a, b) in fields {
-            if (a - b).abs() > EPSILON {
-                return false;
-            }
-        }
-
-        true
+        let diff = self - other;
+        let sum: f64 = [diff.0, diff.1, diff.2].iter().sum();
+        sum.abs() > f64::EPSILON
     }
 }
 
-impl Add for Tuple {
+impl Add for &Tuple {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
+    fn add(self, other: Self) -> Self::Output {
         Self(self.0 + other.0, self.1 + other.1, self.2 + other.2)
     }
 }
@@ -46,14 +36,14 @@ impl Coordinate {
 
 impl PartialEq for Coordinate {
     fn eq(&self, other: &Self) -> bool {
-        self.tuple == other.tuple && (self.w - other.w).abs() <= EPSILON
+        self.tuple == other.tuple && (self.w - other.w).abs() <= f64::EPSILON
     }
 }
 
 impl Add for Coordinate {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
+    fn add(self, other: Self) -> Self::Output {
         Self {
             tuple: self.tuple + other.tuple,
             w: self.w + other.w,
@@ -82,7 +72,7 @@ impl Vector {
 impl Add for Vector {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
+    fn add(self, other: Self) -> Self::Output {
         Self(self.0 + other.0)
     }
 }
@@ -90,7 +80,7 @@ impl Add for Vector {
 impl Add<Point> for Vector {
     type Output = Point;
 
-    fn add(self, other: Point) -> Point {
+    fn add(self, other: Point) -> Self::Output {
         Point(self.0 + other.0)
     }
 }
@@ -107,7 +97,7 @@ impl Color {
 impl Add for Color {
     type Output = Self;
 
-    fn Add(self, Other: Self) -> Self {
+    fn add(self, other: Self) -> Self::Output {
         Self(self.0 + other.0)
     }
 }
